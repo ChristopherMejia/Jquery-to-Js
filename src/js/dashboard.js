3143fe85233becff5819c112b05c54f949cc158c
Christopher Mejia <chis.mejia.garcia@gmail.com>
2020-12-22 18:52:25 -0600

    add selectors with js

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -10,6 +10,22 @@
     const animationList = await getData('https://yts.mx/api/v2/list_movies.json?genre=animation')
     console.log(actionList, dramaList, animationList);
   
+      //const $home = $('.modal');
+    const $modal = document.getElementById('modal');
+    const $overlay = document.getElementById('overlay');
+    const $hideModal = document.getElementById('hide-modal');
+    
+    const $actionContainer = document.querySelector('#action');
+    const $dramaContainer = document.getElementById('drama');
+    const $animationContainer = document.getElementById('animation');
+    
+    const $featuringContainer = document.getElementById('featuring');
+    const $form = document.getElementById('form');
+    const $home = document.getElementById('home');
+    
+    const $modalTitle = $modal.querySelector('h1');
+    const $modalImage = $modal.querySelector('img');
+    const $modalDescription = $modal.querySelector('p');
   })()
 
   
